38c323d535b3815e4dc9bcd23bfa3e8a18ea8258
Anurag Arwalkar <anurag.arwalkar@globant.com>
2024-05-10 18:17:43 +0530

    added

--- dist/src/MyButton.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export type ButtonProps = {
-    onPress: () => void;
-    text: string;
-    color?: string;
-    textColor?: string;
-};
-export declare const MyButton: ({ text, onPress, color, textColor }: ButtonProps) => React.JSX.Element;
